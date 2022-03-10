33100d743d37b1869beeda223889748e5e5dde4f
Ben Blanc <Ben.Blanc@GeorgianCollege.ca>
2022-03-10 14:54:45 -0500

    Add project files.

---
+++ comp2084Winter2022ThursdayCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using comp2084Winter2022ThursdayCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace comp2084Winter2022ThursdayCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        //add a DbSet<TableName> TableName {get; set}
+        //db.Semesters
+
+        DbSet<Employee> employees { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ comp2084Winter2022ThursdayCore/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comp2084Winter2022ThursdayCore.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Custom Error Message")]
+        [Column(Order = 0, TypeName = "int")]
+        [Range(1000000, 9999999)]
+        public int EmployeeID { get; set; }
+        [Required]
+        [Column(Order = 1, TypeName = "nvarchar(50)")]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [Column(Order = 0, TypeName = "nvarchar(10)")]
+        public string EmployeePosition { get; set; }
+
+        [Required]
+        [Column(Order = 0, TypeName = "int")]
+        public int EmployeeAge { get; set; }
+    }
+}
